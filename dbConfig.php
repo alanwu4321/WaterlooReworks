72b2ffc8e55e973b57ae8539115f32d0b9884c6c
Alan Wu <alwu@Alans-MacBook-Pro.local>
2019-07-23 02:16:58 -0400

    Fix db connection

--- dbConfig.php
+++ dbConfig.php
@@ -1,15 +1,15 @@
 <?php
 //Development
-$dbhost = '127.0.0.1:3306';
-$dbuser = 'root';
-$dbpassword = '';
-$dbname = 'ac2wu';
-
-//Deployment
-// $dbhost = 'localhost';
-// $dbuser = 'ac2wu';
-// $dbpassword = 'X98077s007007$$$';
+// $dbhost = '127.0.0.1:3306';
+// $dbuser = 'root';
+// $dbpassword = '';
 // $dbname = 'ac2wu';
+
+// Production
+$dbhost = 'localhost';
+$dbuser = 'ac2wu';
+$dbpassword = 'X98077s007007$$$';
+$dbname = 'ac2wu';
 
 // (2) mysqli connection variable
 $mysqli = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
